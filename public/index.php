8a7673ea2a39a1c9a355dc2a1be75d89650c96d0
Saifil Momin <saifilmomin9@gmail.com>
2018-03-18 17:57:12 +0530

    work area

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Saifil
+ * Date: 18/03/18
+ * Time: 5:51 PM
+ */
---
+++ public/manage_content.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Saifil
+ * Date: 18/03/18
+ * Time: 5:53 PM
+ */
